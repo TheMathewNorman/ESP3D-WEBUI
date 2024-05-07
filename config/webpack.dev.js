98806c467f73d628727c6e2bd5c40ab34ed9216e
Mathew Norman <spam@matnorman.com>
2024-05-07 17:43:52 +1000

    Initial commit

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -17,9 +17,10 @@ module.exports = {
         subtarget
       ),
     },
+    extsions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
   },
   mode: "development", // this will trigger some webpack default stuffs for dev
-  entry: path.resolve(__dirname, "../src/index.js"), // if not set, default path to './src/index.js'. Accepts an object with multiple key-value pairs, with key as your custom bundle filename(substituting the [name]), and value as the corresponding file path
+  entry: path.resolve(__dirname, "../src/index.ts"), // if not set, default path to './src/index.js'. Accepts an object with multiple key-value pairs, with key as your custom bundle filename(substituting the [name]), and value as the corresponding file path
   output: {
     filename: "[name].bundle.js", // [name] will take whatever the input filename is. defaults to 'main' if only a single entry value
     path: path.resolve(__dirname, "../dist"), // the folder containing you final dist/build files. Default to './dist'
@@ -82,6 +83,8 @@ module.exports = {
           },
         ],
       },
+      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+      { test: /\.tsx?$/, loader: "ts-loader" },
     ],
   },
 };
